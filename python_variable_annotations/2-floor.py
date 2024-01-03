6795c619069683effb2dc93eabba181cf744a0f1
lgallogomez <l.gallogomez@gmail.com>
2024-01-03 14:30:58 -0500

    3th function created with main

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 
-"""
+'''
 function takes a float and returns n as integer
-"""
+'''
 
 
 def floor(n: float) -> int:
-    #function takes a float and returns n as integer
+    '''
+    function takes a float and returns n as integer
+    '''
     return int(n)
