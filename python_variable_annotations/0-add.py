39acc07a98936a15163829f3fde5b1a774fa1650
lgallogomez <l.gallogomez@gmail.com>
2024-01-03 14:08:37 -0500

    moved readme file inside dir

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
 ''' 
-Type-annotated function
+Type-annotated function add
 '''
 
 def add(a: float, b: float) -> float:
+    '''
+    function adds 2 floats
+    '''
     return a + b
