be05b03f53a782f4f9c10153f29475e8ca99cc4e
lgallogomez <l.gallogomez@gmail.com>
2024-01-04 11:41:00 -0500

    changed lines 3, 5, 13, 15

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -9,7 +9,6 @@ program has a single function
 from typing import Tuple
 from typing import Union
 
-
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     '''
     function takes args & returns a Tuple
