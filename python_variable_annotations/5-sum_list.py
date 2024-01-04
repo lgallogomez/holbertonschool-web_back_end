ec6c2be663816b7af1418a716e604a896dd0caf9
lgallogomez <l.gallogomez@gmail.com>
2024-01-03 20:08:16 -0500

    created 5th function and main file

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+program has a single function
+"""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    function adds floats of a list
+    """
+    return sum(input_list)
