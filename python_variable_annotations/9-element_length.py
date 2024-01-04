75f04787e75cacd11da178e0c9fbb20719b613c8
lgallogomez <l.gallogomez@gmail.com>
2024-01-04 16:10:20 -0500

    9th function and main created

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""
+program has a single function
+"""
+
+from typing import Iterable, List, Tuple, Sequence
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    gets iterable list resuts list with a tuple
+    """
+    return [(i, len(i)) for i in lst]
