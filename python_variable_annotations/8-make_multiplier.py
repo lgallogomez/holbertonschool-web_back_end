8f26c83681adfb324ee348a514692e2570719a44
lgallogomez <l.gallogomez@gmail.com>
2024-01-04 14:46:42 -0500

    erased comments on inner funct

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
-'''
+"""
 program has a single function
-'''
+"""
 
 
 from typing import Callable
 
 
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
-    '''
+    """
     function returns another function
-    '''
+    """
     def mult(number: float):
-        return (number * multiplier)
-    
-    return (mult)
+        return number * multiplier
+    return mult
