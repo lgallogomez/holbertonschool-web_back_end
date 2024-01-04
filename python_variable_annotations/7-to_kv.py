d192ad534b3eec1d9cae8d3a414fe8190e9626e4
lgallogomez <l.gallogomez@gmail.com>
2024-01-04 11:57:40 -0500

    created variable tupla

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python3
 
-"""
+'''
 program has a single function
-"""
+'''
 
 
 from typing import Tuple, Union
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-	"""
-	function returns Tuple, first str, second float of square value of v
-	"""
+	'''
+	function returns Tuple, first str, second float
+	of square value of v
+	'''
 	tupla: Tuple[str, float] = (k, v*v)
 	return tupla
