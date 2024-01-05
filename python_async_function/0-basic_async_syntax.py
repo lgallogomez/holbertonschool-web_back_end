6add871476dc8786f92f55d0a8414558066fd7a7
lgallogomez <l.gallogomez@gmail.com>
2024-01-05 12:43:04 -0500

    created directory and files

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -8,7 +8,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay = 10):
+async def wait_random(max_delay: int = 10):
     '''
     waits for random delay between 0 & max delay
     and returns it
