e660c1c3a843e6dbf5b2d72c69394a994b9454d6
lgallogomez <l.gallogomez@gmail.com>
2024-01-05 17:38:23 -0500

    created 3th function

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+'''
+programs contains a function
+'''
+
+
+import time
+from tracemalloc import start
+from typing import List
+import asyncio
+
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    '''
+    measures execution time for wait_n function
+    '''
+    start_time = time.perf_counter()
+    await wait_n(n, max_delay)
+    finished_time = time.perf_counter()
+    total_time = finished_time - start_time
+    return total_time / n
