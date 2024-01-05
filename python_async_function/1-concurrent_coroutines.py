25f45b44b5519665efcc6b5b270d2253a6fc0578
lgallogomez <l.gallogomez@gmail.com>
2024-01-05 15:51:35 -0500

    using while loop

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -5,7 +5,6 @@ programs contains a coroutine
 '''
 
 
-from hmac import new
 from typing import List
 
 
@@ -16,15 +15,12 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     '''
     uses wait_rando n times
     '''
-    my_list = []
+    delays_list = []
+    insertion_point = 0
     for i in range(n):
         delay_time = await wait_random(max_delay)
-        new_value = delay_time
-        if not my_list:
-            my_list.append(new_value)
-        for element in my_list:
-            if new_value < element:
-                my_list.insert(my_list.index(delay_time), new_value)
-            else:
-                my_list.insert(my_list.index(element) + 1, new_value)
-    return my_list
+        
+        while insertion_point < len(delays_list) and delay_time > delays_list[insertion_point]:
+            insertion_point += 1
+        delays_list.insert(insertion_point, delay_time)
+    return delays_list
