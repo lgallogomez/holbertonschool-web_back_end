62c664aba4bef1b05c0aa37e4eb0a5db49e49047
lgallogomez <l.gallogomez@gmail.com>
2024-01-05 14:41:12 -0500

    erased sort

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -13,11 +13,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     '''
-    does n times
+    uses wait_rando n times
     '''
     my_list = []
     for i in range(n):
         delay_time = await wait_random(max_delay)
         my_list.append(delay_time)
-    my_list.sort()
     return my_list
