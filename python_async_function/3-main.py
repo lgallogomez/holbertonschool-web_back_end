4d05f5a1a43fbc102d33f3b8f531f1c3fa0cfcb1
lgallogomez <l.gallogomez@gmail.com>
2024-01-05 19:31:53 -0500

    added max_delay paramter when calling function

--- python_async_function/3-main.py
+++ python_async_function/3-main.py
@@ -8,6 +8,6 @@ task_wait_random = __import__('3-tasks').task_wait_random
 async def test(max_delay: int) -> float:
     task = task_wait_random(max_delay)
     await task
-    print(task)
+    print(task.__class__)
 
 asyncio.run(test(5))
