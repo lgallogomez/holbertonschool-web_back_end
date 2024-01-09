87ff6fbc22d52a42a4085a06be0290b046b449b6
lgallogomez <l.gallogomez@gmail.com>
2024-01-09 16:43:19 -0500

    erased white spaces

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -16,12 +16,10 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     uses wait_rando n times
     '''
     delays_list = []
-
     all_tasks = [wait_random(max_delay) for i in range(n)]
-    
-    #starts all wait_random iterations at same time concurrently
+    # list comprehension doesnt start the function
     for one_task in asyncio.as_completed(all_tasks):
+        # starts all wait_random iterations at same time concurrently
         one_delayed_time = await one_task
-        delays_list.append(one_delayed_time)
-    
+        delays_list.append(one_delayed_time) 
     return delays_list
