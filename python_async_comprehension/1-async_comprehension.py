011d0a883989181c28540988d9a7f3f4cc3af7d6
lgallogomez <l.gallogomez@gmail.com>
2024-01-12 15:13:36 -0500

    created pycodestyle

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,16 @@ coroutine collects 10 rand numbers using the async
 comprehensing over async_generator that returns
 10 rand nums
 '''
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
-import asyncio
 
-async def async_comprehension():
+
+async def async_comprehension() -> List:
+    '''
+    calls function async_generator, iterates in async mode
+    the number of times the func is programmed, returns a
+    list with rand numbers.
+    '''
     numbers = [i async for i in async_generator()]
     return numbers
