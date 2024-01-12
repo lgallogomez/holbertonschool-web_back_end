b2b87c75b572bfb3dcd6a4bc91c11b360cc8343a
lgallogomez <l.gallogomez@gmail.com>
2024-01-12 11:03:09 -0500

    created 4th file

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+'''
+programs contains a coroutine
+'''
+
+import asyncio
+from typing import List
+
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    uses wait_rando n times
+    '''
+    delays_list = []
+    all_tasks = [wait_random(max_delay) for i in range(n)]
+    # list comprehension doesnt start the function
+    for one_task in asyncio.as_completed(all_tasks):
+        # starts all wait_random iterations at same time concurrently
+        one_delayed_time = await one_task
+        delays_list.append(one_delayed_time)    
+    return delays_list
+    
