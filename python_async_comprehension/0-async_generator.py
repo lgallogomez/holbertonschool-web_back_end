6085d7c1c71116add8fc7b09351aa6f6ccfd7c72
lgallogomez <l.gallogomez@gmail.com>
2024-01-12 14:07:52 -0500

    created dir and 1st files

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import asyncio
+import random
+
+'''
+corotutine loops 10 times, waits 1 sec, and yield a 
+random # between 0 - 10
+'''
+
+
+async def async_generator():
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
