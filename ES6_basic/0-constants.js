6b2d693fd196f159cd0afd7a09cc2a5a2b262ab5
lgallogomez <l.gallogomez@gmail.com>
2024-01-23 12:53:49 -0500

    changed tab identation from 4 to 2

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -1,15 +1,15 @@
 export function taskFirst() {
-    const task = 'I refer const when I can.';
-    return task
+  const task = 'I refer const when I can.';
+  return task
 }
 
 export function getLast() {
-    return ' us okay';
+  return ' us okay';
 }
 
 export function taskNext() {
-    let combination =  'But sometimes let';
-    combination += getLast();
+  let combination =  'But sometimes let';
+  combination += getLast();
 
-    return combination;
+  return combination;
 }
