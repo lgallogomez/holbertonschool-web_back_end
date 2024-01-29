e46108331c5ea7d99ef9ece1af6584148edaa55d
lgallogomez <l.gallogomez@gmail.com>
2024-01-28 21:40:10 -0500

    updated 10th file creating new array, not altering current one

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,10 +1,9 @@
 export default function appendToEachArrayValue(array, appendString) {
+  const newArray = [];
   let idx = 0;
-  let value;
-  for (value of array) {
-    value = array[idx];
-    array[idx] = appendString + value;
+  for (const value of array) {
+    newArray[idx] = appendString + value;
     idx += 1;
   }
-  return array;
+  return newArray;
 }
