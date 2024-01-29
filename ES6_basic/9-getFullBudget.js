b90cff3e1afb19d81725c85be1f4c25b17e829b1
lgallogomez <l.gallogomez@gmail.com>
2024-01-28 19:58:50 -0500

    updated 9th file

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,12 +1,11 @@
 import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
+  const budget = getBudgetObject(income, gdp, capita);
   const fullBudget = {
-    income,
-    gdp,
-    capita,
+    ...budget,
     getIncomeInDollars: (income) => `$${income}`,
-    getIncomeInEuros: (income) => `${income} euros`
-    }
-  return fullBudget
+    getIncomeInEuros: (income) => `${income} euros`,
+  };
+  return fullBudget;
 }
