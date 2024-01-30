d0689f01a4982ca31a4a7520ae1adf3aac56db4b
lgallogomez <l.gallogomez@gmail.com>
2024-01-30 18:00:20 -0500

    created 4th file

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,31 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (!(currency instanceof Currency)) {
+      throw (TypeError('Currency must be an instance of class Currency'));
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return (this._amount);
+  }
+
+  get currency() {
+    return (this._currency);
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return (`${this._amount} ${this._currency.displayFullCurrency()}`);
+  }
+}
