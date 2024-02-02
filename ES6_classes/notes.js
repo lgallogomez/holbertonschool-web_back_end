126b0686181c19f367bc5cf669aea51bb146eb76
lgallogomez <l.gallogomez@gmail.com>
2024-02-01 21:32:27 -0500

    updated sqft property

---
+++ ES6_classes/notes.js
@@ -0,0 +1,22 @@
+/* paradigmes, ways to do an algorithm
+
+OBJECT ORIENTED PROGRAMMING:
+Seeks order things
+represent the world we live in
+reuse code - once a mold is made, you can create x number of objects of that mold (class)
+
+ways to resolve problems
+ 
+PLATZI
+objects:
+students :
+  methods: 
+  add_course()
+  
+courses
+objects 
+
+instance of an object student:
+miguel
+  methods: course to add in miguel's list 
+*/
