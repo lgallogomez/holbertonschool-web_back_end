13f7adaa16cbcdfeca8ca17bf0c44c0766f8196e
lgallogomez <l.gallogomez@gmail.com>
2024-02-05 11:35:14 -0500

    lint changes

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,6 @@
-//revisar que cada edificio 
 export default class Building {
   constructor(sqft) {
-    if (new.target !== Building && typeof(this.evacuationWarningMessage) !== 'function') { //new.target or this.constructor = who's my created me.
+    if (new.target !== Building && typeof (this.evacuationWarningMessage) !== 'function') {
       throw new Error('Class extending Building must override evacuationWarningMessage');
     }
     this._sqft = sqft;
